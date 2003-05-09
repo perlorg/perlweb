63d9dcc94c21d1eccd3427f4a3edc2e86e0fa44f
Ask Bjoern Hansen <ask@develooper.com>
2003-05-09 11:06:56 +0000

    add documentation, uuuh.
    git-svn-id: https://svn.develooper.com/combust/backend/trunk@70 516b4451-ceb8-0310-875f-b17149c99682

--- lib/Combust/Cache.pm
+++ lib/Combust/Cache.pm
@@ -98,5 +98,100 @@ sub store {
 	  );
 }
 
-
 1;
+__END__
+=head1 NAME
+  Combust::Cache - Caching module
+
+=head1 SYNOPSIS
+
+  my $cache = Combust::Cache->new(type => 'dev.perl');
+
+  $cache->store(id => "html;/index.html",
+                data => $output,
+                meta_data => { content_type => 'text/html' });
+
+  my $data = $cache->fetch(id => "html;/index.html");
+  print $data->{data},
+         " with type ", $data->{meta_data}->{content_type},
+         " created on ", $data->{created};
+
+
+  # store complex data using the id specified in fetch
+  $data = $cache->fetch(id => "rss;somefeed");
+  $cache->store(data => { rss => $rss, meta => $metadata }); 
+ 
+=head1 CONSTRUCTOR
+
+=over 4
+
+=item new
+
+requires one parameter, "type".  The type is the higest level in the
+cache key name space and should be used to separate use of the cache
+in different parts of the system.
+
+Examples would be "rssloader", "www.site.com-html", "pod converter
+cache".
+
+=back
+
+=head1 METHODS
+
+=over 4
+
+=item store( id => C<ID>, purge_key => C<PURGE_KEY>, data => C<DATA>, meta_data => C<METADATA> )
+
+Store data to the cache.
+
+C<DATA> is the data to be stored.  Only required field.  Will be
+serialized with Storable if it's a reference.
+
+C<ID> is the cache key.  Optional, will use the last id passed to the
+fetch method if not specified here.
+
+C<META_DATA>: Hash reference to meta data stored with the data entry.
+Can for example contain the mime type of the data.
+
+The C<PURGE_KEY> is like C<ID> but only used for purging data.  Can be
+used to easily purge part of the cache when data is updated.
+
+=item fetch ( id => C<ID> )
+
+Fetches data from the cache.  The id is stored and reused for a
+subsequent call to C<store>.  Returns a hash reference with the
+following data
+
+=over 4
+
+=item C<data>
+
+Whatever was stored. 
+
+=item C<meta_data>
+
+The hash reference that was stored; otherwise an empty hash ref.
+
+=item C<created_timestamp>
+
+When the cache entry was created; as a unix timestamp.
+
+=item C<created>
+
+When the cache entry was created; in MySQL datatime format.
+
+=back
+
+=HEAD1 TODO
+
+=over 4
+
+=item Method to purge entries with a certain purge_key and maybe force expire entries by id too.
+
+=back
+
+=cut
+
+
+
+
