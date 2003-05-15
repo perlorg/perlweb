daa7924131e6357d1d2bfa25727f4b2bd468ae18
Ask Bjoern Hansen <ask@develooper.com>
2003-05-15 08:43:00 +0000

    template toolkit based httpd.conf
    use combust.conf for servername configuration
    
    
    git-svn-id: https://svn.develooper.com/combust/backend/trunk@86 516b4451-ceb8-0310-875f-b17149c99682

---
+++ lib/Combust/Config.pm
@@ -0,0 +1,71 @@
+package Combust::Config;
+use strict;
+use Config::Simple;
+use Data::Dumper qw();
+use Sys::Hostname qw(hostname);
+
+my $file = "$ENV{CBROOT}/combust.conf";
+
+#my %Config;
+#Config::Simple->import_from($file, \%Config);
+
+my $cfg = new Config::Simple($file);
+
+my %Config = $cfg->vars();
+warn Data::Dumper->Dump([\$cfg],[qw(Config)]);
+warn Data::Dumper->Dump([\%Config],[qw(Config)]);
+
+my $singleton;
+
+sub new {
+  return $singleton if $singleton;
+  $singleton = shift->_new(@_);
+}
+
+sub _new {
+  my ($proto, %args) = (shift, @_);
+  my $class = ref $proto || $proto;
+  my $type  = $args{type}|| '';
+  my $self = { };
+  $self = bless( $self, $class);
+}
+
+sub site {
+  my $self = shift;
+  return $self->{_site} if $self->{_site};
+  my $h = {};
+
+  for my $site ($self->sites_list) {
+    my $sd = $cfg->param(-block=>$site);
+    # FIXME: should resolve refs and stuff ...
+    $h->{$site} = $sd;
+  }
+
+  warn Data::Dumper->Dump([\$h], [qw(x)]);
+
+  $self->{_site} = $h; 
+}
+
+sub sites_list { 
+  my $sites = $cfg->param('sites');
+  ref $sites ? @$sites : ($sites);
+}
+
+sub sites {
+  #warn Data::Dumper->Dump([\$sites], [qw(sites)]);
+  +{ map { $_ => 1 } shift->sites_list };
+}
+
+sub servername {
+  $cfg->param('servername') || hostname || 'combust-server';
+}
+
+sub port {
+  $cfg->param('port') || 8225;
+}
+
+#sub config {
+#  $cfg;
+#}
+
+1;
