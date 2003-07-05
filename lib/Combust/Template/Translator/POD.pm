7058020cee8aa210f83483104c8ab58f515c93a5
Robert Spier <rspier@pobox.com>
2003-07-05 00:32:34 +0000

    backend support for dev better pod TITLE stuff
    git-svn-id: https://svn.develooper.com/combust/backend/trunk@135 516b4451-ceb8-0310-875f-b17149c99682

--- lib/Combust/Template/Translator/POD.pm
+++ lib/Combust/Template/Translator/POD.pm
@@ -1,3 +1,55 @@
+
+package Pod::Simple::HTML::Combust;
+use base qw(Pod::Simple::HTML);
+
+# stub out the Pod::Simple::HTML do_beginning and do_end methods which
+# deal with the page header and footer.
+
+sub do_beginning {
+  return 1;
+}
+
+sub do_end {
+  return 1;
+}
+
+# a temporary fix until Pod::Simple::PullParser catches up
+sub get_title {
+  my $self = shift;
+  my $max_tokens = 25;
+  my $title;
+  my @to_unget;
+  my $state = 0;
+  my $depth = 0;
+
+  while($max_tokens-- and defined(my $token = $self->get_token)) {
+    push @to_unget, $token;
+    if ($state == 0) {
+      ++$state if $token->is_start and $token->tagname eq 'head1';
+    }
+    elsif($state == 1) {
+      --$state if $token->is_end and $token->tagname eq 'head1';
+      ++$state if $token->is_text and ($token->text eq 'TITLE' or $token->text eq "NAME");
+    }
+    elsif($state == 2) {
+      ++$state, $depth=0 if $token->is_end and $token->tagname eq 'head1';
+    }
+    elsif($state == 3) {
+      $depth++ if $token->is_start;
+      last if $token->is_end and --$depth == 0;
+      $title .= $token->text if $token->is_text;
+    }
+  }
+
+  # Put it all back:
+  $self->unget_token(@to_unget);
+
+  return '' unless defined $title;
+  $title =~ s/^\s+//;
+  return $title;
+}
+
+
 package Combust::Template::Translator::POD;
 use strict;
 use Template::Document;
@@ -14,12 +66,13 @@ sub translate {
   my ($self, $data) = @_;
 
   my $out;
-  my $psh = new Pod::Simple::HTML();
+  my $psh = new Pod::Simple::HTML::Combust();
+  $psh->no_errata_section(1);
+  $psh->complain_stderr(1);
   $psh->output_string( \$out );
-  $psh->parse_string_document( $data->{text} );
-
-  # cheat and extract title (hacky!)
-  my ($title) = $out =~ m!<title>(.+?)</title>!i;
+  $psh->set_source( \( $data->{text} ) );
+  my $title = $psh->get_title_short( );
+  $psh->run;
 
   Template::Document->new({
 			   BLOCK => sub { $out },
