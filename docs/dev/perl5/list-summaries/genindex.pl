e929bc5b82b525a751f35a1f8e152c3b215dcc40
Rafael Garcia-Suarez <rgs@consttype.org>
2003-11-17 20:13:58 +0000

    Add a script to generate a nicer index for the P5P summaries. Regenerate this index.

---
+++ docs/dev/perl5/list-summaries/genindex.pl
@@ -0,0 +1,32 @@
+#!perl
+# Generates index.html
+use strict;
+use warnings;
+open my $index, '>', 'index.html' or die "Can't open index.html: $!\n";
+print $index <<'**HEAD**';
+[% page.title = "Perl5 List Summaries" %]
+
+By 
+<A HREF="mailto:rgarciasuarez@free.fr">Rafael Garcia-Suarez</A>
+
+<UL>
+**HEAD**
+for my $pod (sort <*/*.pod>) {
+    open my $fh, '<', $pod or die "Can't read $pod: $!\n";
+    my $head = <$fh>;
+    close $fh;
+    if ($head !~ /^=head1 This Week on perl5-porters \((.*)\)/i) {
+	warn "$pod malformed\n";
+	next;
+    }
+    else {
+	my $date = $1;
+	$pod =~ s/pod$/html/;
+	print $index <<"**HTML**";
+<LI><A HREF="$pod">$date</A>
+**HTML**
+    }
+}
+print $index "</UL>\n";
+close $index;
+__END__
