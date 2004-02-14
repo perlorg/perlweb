61899725488798bd8f56606db76301b9d169d27b
Ask Bjoern Hansen <ask@develooper.com>
2004-02-14 01:16:19 +0000

    add ldap tmpl tweak httpd settings modify Class::DBI settings for CPANratings
    git-svn-id: https://svn.develooper.com/combust/trunk@216 516b4451-ceb8-0310-875f-b17149c99682

--- lib/CPANRatings/Model/DBI.pm
+++ lib/CPANRatings/Model/DBI.pm
@@ -1,6 +1,6 @@
 package CPANRatings::Model::DBI;
 use strict;
-use base qw(Class::DBI::mysql);
+use base qw(Class::DBI);
 use Develooper::DB qw(db_open);
 
 sub db_Main { return db_open('combust') }
