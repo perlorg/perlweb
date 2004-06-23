f56a744e90835234bd0a00ac39402f96cb6b8ab1
Ask Bjoern Hansen <ask@develooper.com>
2004-06-23 16:00:47 +0000

    make an "Extras" bundle for Good Stuff To Have when you make Combust applications
    git-svn-id: https://svn.develooper.com/combust/trunk@251 516b4451-ceb8-0310-875f-b17149c99682

---
+++ misc/Bundle-Combust/Extras.pm
@@ -0,0 +1,33 @@
+package Bundle::Combust::Extras;
+
+=head1 CONTENTS
+
+DBD::SQLIte
+
+Class::DBI
+
+CGI::Untaint
+
+CGI::Untaint::ipaddress
+
+CGI::Untaint::email
+
+CGI::Untaint::date
+
+Class::DBI::mysql
+
+Class::DBI::Untaint
+
+Class::DBI::FromCGI
+
+Mail::Sendmail
+
+Digest::SHA1
+
+=cut
+
+# more untaint modules
+# 
+
+
+1;
