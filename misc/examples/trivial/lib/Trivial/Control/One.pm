888125ae6f3220595a1ef8795a656cf2ac3d1524
Robert Spier <rspier@pobox.com>
2005-07-21 21:59:05 +0000

    r269@bear:  rspier | 2005-07-21T21:58:48.965405Z  commit trivial example site
    git-svn-id: https://svn.develooper.com/combust/trunk@378 516b4451-ceb8-0310-875f-b17149c99682

---
+++ misc/examples/trivial/lib/Trivial/Control/One.pm
@@ -0,0 +1,18 @@
+package Test::Control::One;
+
+use base 'Combust::Control';
+
+sub handler ($$) {
+  my ($self, $r) = @_;
+
+  $self->tpl_param('now', scalar localtime );
+  my $output = $self->evaluate_template('one');
+  if ($@) {
+    $r->pnotes('error', $@);
+    return 404 if $@ =~ m/: not found/;
+    return 500; 
+  }
+  $self->send_output($output, 'text/html');
+}
+
+1;
