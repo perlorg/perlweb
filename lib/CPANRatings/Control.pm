62e63fbef5ff9a090d92fefa5f690f6bb20461a3
Ask Bjoern Hansen <ask@develooper.com>
2005-07-21 01:01:24 +0000

    remove old code
    mark data coming out of the reviews tables as utf8
    
    
    git-svn-id: https://svn.develooper.com/combust/trunk@364 516b4451-ceb8-0310-875f-b17149c99682

--- lib/CPANRatings/Control.pm
+++ lib/CPANRatings/Control.pm
@@ -42,22 +42,6 @@ sub init {
   return OK;
 }
 
-sub ___no___send_output {
-  my $class   = shift;
-  my $routput = shift;
-
-  $routput = $$routput if ref $routput;
-
-  return $class->SUPER::send_output(\$routput, @_)
-    if (ref $routput eq "GLOB" or $class->{utf8});
-
-#  binmode STDOUT, ':utf8';
-
-  my $str = Encode::encode('iso-8859-1', $routput, Encode::FB_HTMLCREF);
-
-  $class->SUPER::send_output(\$str, @_);
-}
-
 sub is_logged_in {
   my $self = shift;
   my $user_info = $self->user_info;
--- lib/CPANRatings/Model/DBI.pm
+++ lib/CPANRatings/Model/DBI.pm
@@ -9,5 +9,22 @@ sub dbh {
 
 sub db_Main { 
   my $class = shift;
-  return db_open('combust', {$class->_default_attributes}) }
+  return db_open('combust', {$class->_default_attributes})
+}
+
+sub get {
+  my $self = shift;
+  my $attr = shift;
+  
+  my $data = $self->SUPER::get($attr, @_);
+  my $data2;
+
+  if ($] > 5.007) {
+    #Encode::_utf8_on($data);
+    $data2 = Encode::decode_utf8($data);
+  }
+  
+  return defined $data2 ? $data2 : $data;
+}
+
 1;
