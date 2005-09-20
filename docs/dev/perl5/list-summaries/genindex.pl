1b70028e54cd33fa45cfc3b16c68807a98f5934b
Rafael Garcia-Suarez <rgs@consttype.org>
2005-09-20 08:35:15 +0000

    * Add P5P summary for 12-18 September 2005. * Remove author name from the index list (now that there are multiple authors) * Make genindex.pl cope with new =head1 format

--- docs/dev/perl5/list-summaries/genindex.pl
+++ docs/dev/perl5/list-summaries/genindex.pl
@@ -6,22 +6,20 @@ open my $index, '>', 'index.html' or die "Can't open index.html: $!\n";
 print $index <<'**HEAD**';
 [% page.title = "Perl5 List Summaries" %]
 
-By 
-<!--<A HREF="mailto:rgarciasuarez@free.fr">Rafael Garcia-Suarez</A>-->
-<A HREF="mailto:lannings@who.int">Scott Lanning</A>
-
 <UL>
 **HEAD**
 for my $pod (sort {$b cmp $a} <*/*.pod>) {
     open my $fh, '<', $pod or die "Can't read $pod: $!\n";
     my $head = <$fh>;
     close $fh;
-    if ($head !~ /^=head1 Th[ie]se? (?:Week|Month)s? on perl5-porters \((.*)\)/i) {
+    if ($head !~ /^=head1 Th[ie]se? (?:Week|Month)s? on perl5-porters +-? *(.*)/i) {
 	warn "$pod malformed\n";
 	next;
     }
     else {
 	my $date = $1;
+	$date =~ s/^\(\s*//;
+	$date =~ s/\s*\)$//;
 	$pod =~ s/pod$/html/;
 	print $index <<"**HTML**";
 <LI><A HREF="$pod">$date</A>
