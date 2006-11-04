bdb198360c3d7eaca721043a9f0c427f173816fb
Ask Bjoern Hansen <ask@develooper.com>
2006-11-04 11:34:51 +0000

    make logout work again
    git-svn-id: https://svn.develooper.com/combust/trunk@546 516b4451-ceb8-0310-875f-b17149c99682

--- lib/CPANRatings/Control/Logout.pm
+++ lib/CPANRatings/Control/Logout.pm
@@ -4,9 +4,7 @@ use strict;
 
 sub render {
   my $self = shift;
-  my $return_url = $self->config->base_url('cpanratings');
-  $self->cookie($CPANRatings::Control::cookie_name, 0);
-  return $self->redirect($self->bitcard->logout_url(r => $return_url));
+  $self->logout;
 }
 
 
