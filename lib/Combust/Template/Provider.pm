f807a7fd91aac73cbe294ff54a6c071f15f71133
Ask Bjoern Hansen <ask@develooper.com>
2007-07-17 01:32:35 +0000

    Move our custom code into a methods in C::Template::Provider.  Get rid of the tweaked 2.09 version of Template::Provider.
    git-svn-id: https://svn.develooper.com/combust/trunk@599 516b4451-ceb8-0310-875f-b17149c99682

--- lib/Combust/Template/Provider.pm
+++ lib/Combust/Template/Provider.pm
@@ -1,6 +1,5 @@
 package Combust::Template::Provider;
 use strict;
-#use base qw(Combust::Template::Provider::Base);
 use base qw(Template::Provider);
 
 sub is_directory {
@@ -49,5 +48,80 @@ sub expand_filename {
 	  };
 }
 
+sub _init {
+    my $class = shift;
+    my $params = $_[0];
+    my $self = $class->SUPER::_init(@_);
+    $self->{EXTENSIONS} = $params->{EXTENSIONS} || [];
+    $self;
+}
+
+sub fetch {
+    my ($self, $name) = (shift, @_);
+
+    my ($data, $error) = $self->SUPER::fetch(@_);
+
+    if ($error) {
+        # no extension to rip off ...
+        return ($data, $error) unless $name =~ s/\.[^.]+$//;
+        
+        for my $ext (@{$self->{EXTENSIONS}}) {
+            my $newname = "$name." . $ext->{extension};
+            ($data, $error) = $self->{ INCLUDE_PATH } 
+            ? $self->load($newname) 
+                : (undef, Template::Constants::STATUS_DECLINED);
+            
+            if (defined $data) {
+                $data = {text => $data };
+                ($data, $error) = $ext->{translator}->translate($data);
+                last;
+            }
+        }
+    }
+
+    return ($data, $error); 
+}
+
 
 1;
+
+__END__
+
+=head1 NAME
+
+Combust::Template::Provider - Combust Template::Provider class
+
+=head1 SYNOPSIS
+
+See L<Template::Provider>
+
+=head1 METHODS
+
+=over 4
+
+=item is_directory
+
+Uses the configured include path to find the specified file and
+returns true if it's a directory.
+
+=item expand_filename
+
+Returns the absolute path given the filename (after searching the
+template toolkit include path).
+
+=item _init
+
+Takes an extra parameter, EXTENSIONS.  Otherwise deferes to the
+regular _init method.
+
+=item fetch
+
+If the template object is configured with EXTENSIONS we'll fall-back
+to using the configured translator class to convert the document.  For
+example if there's a "pod" translator and a request for F<foo.html>
+comes in we'll (if such a file doesn't exist) look for F<foo.pod> and
+use the translator to convert.  See L<Combust::Template> and
+L<Combust::Template::Translator::POD> for an example.
+
+
+=back
