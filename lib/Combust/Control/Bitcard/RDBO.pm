552104af2e96d0f5bfaccbcaf9d026c57292afb2
Ask Bjoern Hansen <ask@develooper.com>
2007-12-11 16:32:34 +0000

    make the lookup of username work (for when the user doesn't have a bitcard_id yet)
    git-svn-id: https://svn.develooper.com/combust/trunk@667 516b4451-ceb8-0310-875f-b17149c99682

--- lib/Combust/Control/Bitcard/RDBO.pm
+++ lib/Combust/Control/Bitcard/RDBO.pm
@@ -5,14 +5,21 @@ use base qw(Combust::Control::Bitcard);
 sub _setup_user {
     my ($self, $bc_user) = @_;
     my $user;
-    if ($self->bc_user_class->can('username') and $bc_user->{username}) {
-        ($user) = $self->bc_user_class->fetch( username => $bc_user->{username} );
+
+    my ($object_class) = ( $self->bc_user_class =~ m/(.*)::Manager($|=.*)/ );
+    if ($object_class->can('username') and $bc_user->{username}) {
+        $user = $self->bc_user_class->fetch( username => $bc_user->{username} );
     }
-    $user = $self->bc_user_class->fetch_or_create(bitcard_id => $bc_user->{id}) unless $user;
+
+    unless ($user) {
+        $user = $self->bc_user_class->fetch_or_create(bitcard_id => $bc_user->{id});
+    }
+
     for my $m (qw(username email name)) {
         next unless $user->can($m);
         $user->$m($bc_user->{$m});
     }
+
     $user->bitcard_id($bc_user->{id});
     $user->save;
     return $user;
