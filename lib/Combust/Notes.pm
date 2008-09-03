fe5bfafd89c2e6459bc31c149766a5fdc22c547e
Ask Bjørn Hansen <ask@develooper.com>
2008-09-03 00:34:28 +0000

    make Combust::Notes play nice with Apache2

--- lib/Combust/Notes.pm
+++ lib/Combust/Notes.pm
@@ -6,20 +6,44 @@ use Combust::Constant qw(OK NOT_FOUND);
 use Time::HiRes qw(time); # let's be accurate about this
 use DBI;		  # for DBI::hash()
 
+# TODO:
+#   Move this so it's called from Combust::Control rather than as a PostReadRequest handler
+
+use constant MP2 => ( exists $ENV{MOD_PERL_API_VERSION} and 
+		      $ENV{MOD_PERL_API_VERSION} >= 2 ); 
+
+BEGIN {
+    if (MP2) {
+	require Apache2::Request;
+	require Apache2::Connection;
+    }
+    else {
+	require Apache::Request;
+    }
+}
+
+
 sub handler {
-  my $r = Apache::Request->instance( shift );
+  my $r = shift;
+
+  if (MP2) {
+    $r = Apache2::Request->new( $r );
+  }
+  else {
+    $r = Apache::Request->instance( $r );
+  }
 
   my $ip      = $r->connection->remote_ip;
   my $param   = $r->param;
 
-  my $ua = $r->header_in('User-Agent') || '';
+  my $ua = $r->headers_in->{'User-Agent'} || '';
 
   my $req_domain = ($r->hostname =~ m/([^\.]+\.[^\.]+)$/)[0];
 
   my %combust_notes = (
     remote_ip	=> $ip				|| '-',
     user_agent	=> $ua,
-    referer	=> $r->header_in('Referer')	|| '',
+    referer	=> $r->headers_in->{'Referer'}	|| '',
     user_id	=> 'none',
     'time'	=> time(),
     param	=> $param,
