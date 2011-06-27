7a7e8df56ad3dcfeea4689eb5f6d6cbc81d627bc
Ask Bjørn Hansen <ask@develooper.com>
2011-06-26 22:14:56 -0700

    Better cache headers on 'external.png' file

--- docs/static/js/leo.js
+++ docs/static/js/leo.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
         // $('.ext').filter(function(obj) {
         //     this.target="_blank";
         //     return 1;
-        // }).after(' <img src="/i/external.png" alt="external link"/>');
-        //       
+        // }).after(' <img src="/i/external.v1.png" alt="external link"/>');
+        //
         // $("a:not(img)").filter(function(obj) {
         //     this.target="_blank";
         //     return 1;
-        // }).after(' <img src="/i/external.png" alt="external link"/>');
-        //       
-        
-        
+        // }).after(' <img src="/i/external.v1.png" alt="external link"/>');
+        //
+
+
         $('#content a').filter(function() {
             // If it's an image, do NOT show icon
             if($(this).find('img').length)
@@ -21,11 +21,11 @@ $(document).ready(function(){
             // If it's external, ok
             if(this.hostname && this.hostname !== location.hostname) {
                 this.target="_blank";
-                return 1;                
+                return 1;
             }
             return 0;
-        }).after('<img class="extlink" src="/i/external.png" alt="external link"/>');
-        
+        }).after('<img class="extlink" src="/i/external.v1.png" alt="external link"/>');
+
         $('#footer a').filter(function() {
             // If it's an image, do NOT show icon
             if($(this).find('img').length)
@@ -33,11 +33,11 @@ $(document).ready(function(){
                 // If it's external, ok
                 if(this.hostname && this.hostname !== location.hostname) {
                     this.target="_blank";
-                    return 1;                
+                    return 1;
                 }
                 return 0;
-        }).after('<img class="extlink" src="/i/external.png" alt="external link"/>');
-            
+        }).after('<img class="extlink" src="/i/external.v1.png" alt="external link"/>');
+
         $('.round').corner("7px top");
 		$('.module').corner("10px top");
 		//$('.button').corner("7px");
