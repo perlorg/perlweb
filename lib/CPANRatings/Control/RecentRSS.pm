92b321ec865599a7ef4f867cc728cef324674cf5
Ask Bjørn Hansen <ask@develooper.com>
2011-07-25 19:15:49 -0400

    Make RSS index work again

--- lib/CPANRatings/Control/RecentRSS.pm
+++ lib/CPANRatings/Control/RecentRSS.pm
@@ -8,7 +8,7 @@ sub render {
 
     $self->tpl_param('header', 'Recent reviews');
 
-    my $reviews = CPANRatings::Model::Reviews->search_recent;
+    my $reviews = $self->schema->review->recent;
     my $output = $self->as_rss($reviews);
     return OK, $output, 'application/rdf+xml';
 }
