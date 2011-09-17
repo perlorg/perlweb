8a4ed520f881b94c8a0d37bd51199c7f326d7f6a
Leo Lapworth <leo@cuckoo.org>
2011-09-17 15:08:37 +0100

    Add the perlfaq to the learn site + script to update

--- docs/learn/static/js/shCore.js
+++ docs/learn/static/js/shCore.js
@@ -1067,7 +1067,7 @@ function processUrls(code)
 		
 		return '<a href="' + m + '">' + m + '</a>' + suffix;
 	});
-	return code.replace(/(<code class="pl keyword">(use|package|require)<\/code> <code class="pl plain">)([A-Za-z0-9\:]+)(.*?<\/code>)/g, '$1<a href="/module/$3">$3</a>$4');
+	return code.replace(/(<code class="pl keyword">(use|package|require)<\/code> <code class="pl plain">)([A-Za-z0-9\:]+)(.*?<\/code>)/g, '$1<a href="https://metacpan.org/module/$3">$3</a>$4');
 };
 
 /**
