18615c9a2a660f684e52c37e5ad2aa3db08eb7bd
Robert <robert@perl.org>
2016-10-08 21:13:43 -0700

    helper script for marking lists as defunct

---
+++ docs/lists/data/defunct.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use JSON;
+use File::Slurp qw(slurp);
+use strict;
+
+# Helper script used to mark lists as defunct.
+#
+# Input: reads from lists.json
+# Output: writes to stdout
+#
+# Usage:
+#  ./defunct.pl $(cat DISABLED | xargs) > lists.json.new
+
+my $data  = from_json(slurp("lists.json"));
+
+for my $list (@ARGV) {
+  if (exists $data->{$list}) {
+    $data->{$list}{defunct} = "1";
+  } elsif (exists $data->{"perl-$list"}) {
+    $data->{"perl-$list"}{defunct} = "1";
+  } else {
+    warn "$list not found\n";
+  }
+}
+
+my $json = new JSON;
+$json->canonical(1); # sort keys
+print $json->pretty->encode($data);
