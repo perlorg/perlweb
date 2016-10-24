01b9648dc4615cbcdf3dde9f9167f50741275d90
Babs Veloso <barbara.veloso@gmail.com>
2016-10-24 11:14:01 -0200

    UI Fixes to footer and more

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,12 +1,12 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 
-  //var navbar = $('.navbar'),
-  //height = navbar.attr('data-height');
-  //$(window).scroll(function() {
-  //  $(this).scrollTop() > height
-  //    ? navbar.addClass("navbar-fixed-top")
-  //    : navbar.removeClass("navbar-fixed-top")
-  //  ;
-  //});
+  var navbar = $('.navbar'),
+  height = navbar.attr('data-height');
+  $(window).scroll(function() {
+    $(this).scrollTop() > height
+      ? navbar.addClass("navbar-fixed-top")
+      : navbar.removeClass("navbar-fixed-top")
+    ;
+  });
 })
