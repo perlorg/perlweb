2cc6d1c3221ccedb24a072668216cca5178061b4
Babs Veloso <barbara.veloso@gmail.com>
2016-10-24 10:59:21 -0200

    Migrating all www templates to the new layout

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
+
+  //var navbar = $('.navbar'),
+  //height = navbar.attr('data-height');
+  //$(window).scroll(function() {
+  //  $(this).scrollTop() > height
+  //    ? navbar.addClass("navbar-fixed-top")
+  //    : navbar.removeClass("navbar-fixed-top")
+  //  ;
+  //});
 })
