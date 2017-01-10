f1d58c59ddae6824088c82b6a85520e6ed8a2739
Ask Bjørn Hansen <ask@develooper.com>
2017-01-10 01:25:03 -0800

    Fix git clone; download RSS files on container startup

--- docker/container-run.sh
+++ docker/container-run.sh
@@ -4,9 +4,12 @@ ls -latr
 if [ -e .git ]; then
 	echo Already has a git checkout
 else
-	git clone --recursive https://github.com/perlorg/perlweb.git .
-	git checkout -b master origin/master
+	git clone -b master \
+            --recursive https://github.com/perlorg/perlweb.git .
 	git submodule update
 fi
+
+# download RSS files etc
+./bin/cron &
 
 ./combust/bin/httpd
