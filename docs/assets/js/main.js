796d36f1e12d8f62e116728d71e756210643d592
Babs Veloso <barbara.veloso@gmail.com>
2017-05-13 17:43:04 +0200

    The header scrolls with the page

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,12 +1,3 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
-
-  var navbar = $('.navbar'),
-  height = navbar.attr('data-height');
-  $(window).scroll(function() {
-    $(this).scrollTop() > height
-      ? navbar.addClass("navbar-fixed-top")
-      : navbar.removeClass("navbar-fixed-top")
-    ;
-  });
 })
