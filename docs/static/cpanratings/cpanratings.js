40d531ca4a82c10724ac97065743ecbaaee51980
Robert <robert@perl.org>
2018-06-08 19:56:40 -0700

    Remove "Was this helpful?" buttons.

--- docs/static/cpanratings/cpanratings.js
+++ docs/static/cpanratings/cpanratings.js
@@ -49,52 +49,5 @@ $(document).ready(function() {
       });
   });
 
-  $("span.helpful").live('click', function(ev) {
-       var review = $(this).parents("div.review");
-       var review_id = review.attr('data-review');
-
-       var thanks_span = review.find("span.thanks");
-       var vote = $(this).text().toLowerCase();
-
-       // $("#reviews").find("span.thanks.done").not(thanks_span).fadeOut();
-
-       thanks_span.removeClass("error");
-       thanks_span.html("Saving ...");
-       thanks_span.show();
-
-       var set_error = function (err) {
-           thanks_span.html(err);
-           thanks_span.addClass("error");
-           thanks_span.addClass("done");
-       };
-
-       $.ajax({ url: "/api/helpful/vote",
-                success: function(data) {
-                    if (data.error) {
-                        thanks_span.html( data.error );
-                        thanks_span.addClass("error");
-                        return;
-                    }
-                    thanks_span.addClass("done");
-                    thanks_span.html(data.message);
-                },
-                data: { "auth_token": global_auth_token,
-                          "vote": vote,
-                          "review_id": review_id
-                      },
-
-                dataType: "json",
-                type: "POST",
-                statusCode: {
-                    412: function() {
-                        set_error("Login before voting");
-                    }
-                },
-                error: function(msg) {
-                    set_error("Whoops - something went wrong!<br>" + msg);
-                }
-             });
-  });
-
   /* images/progress.gif */
 });
